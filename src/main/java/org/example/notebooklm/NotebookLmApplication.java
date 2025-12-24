f1e2a87e9e14c67178f2b66a9092540629c60118
Omer-DN <omerdayan94@gmail.com>
2025-12-24 12:00:00 +0200

    start Notebook Spring Boot project: main app

---
+++ src/main/java/org/example/notebooklm/NotebookLmApplication.java
@@ -0,0 +1,13 @@
+package org.example.notebooklm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NotebookLmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookLmApplication.class, args);
+    }
+}
